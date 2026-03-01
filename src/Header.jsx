7a0437ae8561640ee058b0decafa588c2dbd53c2
LTruter <lariotruter28@gmail.com>
2026-03-01 13:52:43 +0200

    Add a dynamic React description

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,21 @@
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+// function to produce a random number
+function genRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 export default function Header() {
+  // Stores the random description
+  const desc = reactDescriptions[genRandomInt(reactDescriptions.length)];
+
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        {desc} React concepts you will need for almost any app you are going to
+        build!
       </p>
     </header>
   );
